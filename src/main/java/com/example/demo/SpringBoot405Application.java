fae078f70ac7dfed2518872fc614d3a60ef19eb7
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-18 14:18:13 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot405Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot405Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot405Application.class, args);
+    }
+
+}
