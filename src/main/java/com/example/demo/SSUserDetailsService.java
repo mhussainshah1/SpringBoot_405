e55d036e3645bbb60027a4f26320321967a6d6d4
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2023-02-21 00:54:00 -0500

    SpringBoot 3.0

--- src/main/java/com/example/demo/SSUserDetailsService.java
+++ src/main/java/com/example/demo/SSUserDetailsService.java
@@ -7,9 +7,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
-import java.util.HashSet;
-import java.util.Set;
+import jakarta.transaction.Transactional;
+
+import java.util.*;
 
 @Transactional
 @Service
@@ -41,13 +41,21 @@ public class SSUserDetailsService implements UserDetailsService {
         }
     }
 
+    private String[] getRoles(User appUser){
+        List<String> roles = new ArrayList<>();
+        for (Role role : appUser.getRoles()) {
+            roles.add(role.getRole());
+        }
+        return Arrays.copyOf(roles.toArray(),roles.size(), String[].class);
+    }
+
     private Set<GrantedAuthority> getAuthorities(User appUser) {
         Set<GrantedAuthority> authorities = new HashSet<>();
         for (Role role : appUser.getRoles()) {
-            GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(role.getRole());
+            GrantedAuthority grantedAuthority = new SimpleGrantedAuthority("ROLE_" +role.getRole());
             authorities.add(grantedAuthority);
         }
-        System.out.println("User authorities are" + authorities.toString());
+        System.out.println("User authorities are" + authorities);
         return authorities;
     }
 }
