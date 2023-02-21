82dabd78572b0495168eb69184f0c1cf297e0aeb
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2023-02-21 01:04:11 -0500

    SpringBoot 3.0

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -40,13 +41,12 @@ public class SecurityConfiguration {
                 .and()
 
                 .logout()
-                .logoutUrl("/logout")
-//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutUrl("/logout")//.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login?logout").permitAll()
 
                 .and()
 
-                .httpBasic();
+                .httpBasic(Customizer.withDefaults());
         http
                 .csrf().disable();
         http
