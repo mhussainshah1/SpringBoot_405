710cab3034f754acde7fad4ec6066676bda3f28d
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2023-02-23 01:52:50 -0500

    springboot 3.0

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -28,38 +31,44 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests()
-                .requestMatchers("/", "/h2-console/**").permitAll() //.hasAnyRole("USER", "ADMIN")
-                .requestMatchers("/admin").hasRole("ADMIN")//.hasAuthority("ROLE_ADMIN")
-                .anyRequest().authenticated()
-
-                .and()
-
-                .formLogin()
-                .loginPage("/login").permitAll()
-
-                .and()
-
-                .logout()
-                .logoutUrl("/logout")//.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login?logout").permitAll()
-
-                .and()
-
+                .authorizeHttpRequests(authz->authz
+                        .requestMatchers("/", "/h2-console/**").permitAll() //.hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/admin").hasRole("ADMIN")//.hasAuthority("ROLE_ADMIN")
+                        .anyRequest().authenticated())
+                .userDetailsService(userDetailsService)
+//                .userDetailsService(userDetailsService)
+                .formLogin(form -> form
+                        .loginPage("/login")//.failureUrl("/login?error=true")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")//.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll())
                 .httpBasic(Customizer.withDefaults());
         http
-                .csrf().disable();
-        http
-                .headers().frameOptions().disable();
+                .csrf(CsrfConfigurer::disable)
+                .headers(headers -> headers
+                        .frameOptions()
+                        .disable());
         return http.build();
     }
 
-    @Bean
+  /*  @Bean
     public AuthenticationProvider userDetailsServiceBean(BCryptPasswordEncoder passwordEncoder) {
         userDetailsService = new SSUserDetailsService(appUserRepository);
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(passwordEncoder);
         return provider;
+    }*/
+
+    @Bean
+    public AuthenticationManager userDetailsService(HttpSecurity http) throws Exception {
+        userDetailsService = new SSUserDetailsService(appUserRepository);
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
     }
 }
